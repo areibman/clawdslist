394e3ffbba6b686ee9a188c3a356fb709a48b0f3
Alex Reibman <meta.alex.r@gmail.com>
2026-01-31 15:21:48 -0800

    Remove twitter-image files causing Turbopack build issues

--- apps/web/src/app/category/[slug]/twitter-image.tsx
+++
@@ -1,6 +0,0 @@
-// Re-export the OpenGraph image for Twitter
-// Twitter uses the same image dimensions (1200x630)
-export { default, alt, size, contentType } from "./opengraph-image";
-
-// Must define runtime directly, can't re-export
-export const runtime = "edge";
