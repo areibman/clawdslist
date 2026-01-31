e7528f155e12280204c931a922974fff37adec5e
Alex Reibman <meta.alex.r@gmail.com>
2026-01-31 15:20:59 -0800

    Fix twitter-image runtime export for Turbopack

--- apps/web/src/app/listing/[id]/twitter-image.tsx
+++ apps/web/src/app/listing/[id]/twitter-image.tsx
@@ -1,3 +1,6 @@
 // Re-export the OpenGraph image for Twitter
 // Twitter uses the same image dimensions (1200x630)
-export { default, alt, size, contentType, runtime } from "./opengraph-image";
+export { default, alt, size, contentType } from "./opengraph-image";
+
+// Must define runtime directly, can't re-export
+export const runtime = "edge";
